fe54bd9b26414e3937b65d6cac62ba7f347a18bd
alpine delight <alpinedelight@outlook.com>
2020-12-20 21:46:39 +0000

    init schema extension

--- Examples.ps1
+++ Examples.ps1
@@ -9,6 +9,7 @@ $ObjectId = "<EA-SPN-OBJECT-ID-GUID>" # Application object ID, used for permissi
 $clientSecret = "<SPN-SECRET>" # move to key vault, only for testing
 $subscription = "<AZURE-SUBSCRIPTION-NAME>" # name of subscription
 
+$groupOwner = @{'type' = 'servicePrincipals'; 'id' = $ObjectId}
 $ServicePrincipalKey = ConvertTo-SecureString -String $clientSecret -AsPlainText -Force # move to key vault
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential ($ApplicationId, $ServicePrincipalKey)
 Write-Information "Login to Azure as SP: $ApplicationId"
@@ -25,14 +26,19 @@ $TokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$Tena
 $GraphToken = ConvertTo-SecureString -String $TokenResponse.access_token -AsPlainText -Force
 Remove-Variable TokenResponse
 
-$node = 'HM'
-$recursive = $true
-$createIfNotExists = $false
-$accessLevel = 'r-x'
-$groupOwner = @{'type' = 'servicePrincipals'; 'id' = $ObjectId}
+# example uses microsoft graph group schema extensions: https://docs.microsoft.com/en-us/graph/api/group-list?view=graph-rest-1.0&tabs=http
 
-[System.Collections.ArrayList] $outcome = @()
-$paths = 'https://node2curated.blob.core.windows.net/enriched/mynewpath/','https://node2curated.blob.core.windows.net/enriched/citydatacontainer/3ec624b9-bd8b-4caf-97b8-e2e5acc20173Safety/Release/city%3DChicago/','https://node2curated.blob.core.windows.net/enriched/citydatacontainer/3ec624b9-bd8b-4caf-97b8-e2e5acc20173Safety/Release/city=SanFrancisco/','https://node2curated.blob.core.windows.net/enriched/nyctlc/yellow/','https://node2curated.blob.core.windows.net/enriched/nyctlc/green/','https://node1curated.blob.core.windows.net/enriched/diabetes/','https://node1curated.blob.core.windows.net/enriched'
-$paths | ForEach-Object -Process { Write-Host $(Get-Date -Format u)":"$_; $result = Set-DASG-ADLS -node $node -accessLevel $accessLevel -path $_ -groupOwner $groupOwner -recursive $recursive -createIfNotExists $createIfNotExists -browsableFromRoot $true -AzCtx $AzCtx -GraphToken $GraphToken -Verbose; $outcome.Add($result)} -End {Write-Host "Completed at: "$(Get-Date -Format u)}
+$objs = @(
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/mynewpath/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $true; groupowner=$groupOwner; metadata=@{sensitivity="internal"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/nonexistantpath/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="internal"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/citydatacontainer/3ec624b9-bd8b-4caf-97b8-e2e5acc20173Safety/Release/city%3DChicago/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="classified"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/citydatacontainer/3ec624b9-bd8b-4caf-97b8-e2e5acc20173Safety/Release/city=SanFrancisco/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="classified"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/nyctlc/yellow/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="internal"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM2'; path='https://node2curated.blob.core.windows.net/enriched/nyctlc/green/'; recursive=$true; acl='r-x'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="internal"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+    @{node='HM1'; path='https://node1curated.blob.core.windows.net/enriched/diabetes/'; recursive=$true; acl='rwx'; browsableFromRoot = $true; createIfNotExists = $false; groupowner=$groupOwner; metadata=@{sensitivity="classified"; catalogue="https://web.purview.azure.com/datasetid=xyz"}}
+)
+
+[System.Collections.ArrayList] $outcome = @() # create list to store results of DASG calls
+$objs | ForEach-Object -Process { Write-Host $(Get-Date -Format u)":"$_.path; $result = Set-DASG-ADLS -node $_.node -accessLevel $_.acl -path $_.path -groupOwner $_.groupOwner -recursive $_.recursive -createIfNotExists $_.createIfNotExists -browsableFromRoot $_.browsableFromRoot -metadata $_.metadata -AzCtx $AzCtx -GraphToken $GraphToken -Verbose; $outcome.Add($result)} -End {Write-Host "Completed at: "$(Get-Date -Format u)}
 
 $outcome | ForEach {[PSCustomObject]$_} | Format-Table -AutoSize
